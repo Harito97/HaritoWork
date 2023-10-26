51ba031b7c6144451ddead90c7121676bda075d1
Harito97 <harito9703@gmail.com>
2023-10-27 01:13:27 +0700

    parallax animation for x and y dimension in home

--- Home/js/app.js
+++ Home/js/app.js
@@ -6,9 +6,19 @@ window.addEventListener("mousemove", (e) => {
     xValue = e.clientX - window.innerWidth / 2;
     yValue = e.clientY - window.innerHeight / 2;
 
-
     parallax_el.forEach((el) => {
-        el.style.transform = `translateX(calc(-50% + ${xValue}px)) translateY(calc(-50% + ${yValue}px))`;
+        let speedx = el.dataset.speedx, speedy = el.dataset.speedy;
+        el.style.transform = `translateX(calc(-50% + ${-xValue * speedx}px)) translateY(calc(-50% + ${yValue * speedy}px))`;
     });
+});
 
-})
+// Update zValue for z space dimension and rotation and GSAP Animations in the future
+// // GSAP Animations
+// let timeline = gsap.timeline();
+
+// parallax_el.forEach(el => {
+//     timeline.from(el, {
+//         top: `${el.offsetHight / 2 + el.dataset}px`,
+//         duration: 3,
+//     }, 1);
+// });
