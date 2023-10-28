b0f1ca77a02c9ce6fa63a0b8c3486ee288d112a0
Harito97 <harito9703@gmail.com>
2023-10-28 09:43:01 +0700

    update clock UI for homepage

--- Home/js/navbar_behavior.js
+++ Home/js/navbar_behavior.js
@@ -1,49 +1,29 @@
 const navElements = document.querySelectorAll('nav li a');
-const frameURL = document.querySelector('.Tasks iframe')
+const frameURL = document.querySelector('#Extension iframe')
 navElements.forEach((element) => {
-
-  // =======
-//     if (element.innerText == "TIME") {
-//         element.addEventListener('mouseenter', function () {
-//             frameURL.src = "../ClockExtension/clock.html";
-//         });
-//     } else if (element.innerText == "TASKS") {
-//         element.addEventListener('mouseenter', function () {
-//             frameURL.src = "../ToDoExtension/index.html";
-//         });
-//     } else if (element.innerText == "CALCULATOR") {
-//         element.addEventListener('mouseenter', function () {
-//             frameURL.src = "../CalculatorExtension/index.html";
-//         });
-//     } else if (element.innerText == "WEATHER") {
-//         element.addEventListener('mouseenter', function () {
-//             frameURL.src = "../WeatherExtension/index.html";
-//         });
-//     }
-
-//     element.addEventListener('mouseleave', function () {
-//         frameURL.src = "";
-// =======
-    element.addEventListener('mouseenter', function () {
-        // Tải nội dung từ file HTML khác
-        const xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4 && xhr.status === 200) {
-                // Lấy nội dung từ phản hồi của AJAX
-                const response = xhr.responseText;
-                // Hiển thị nội dung vào trung tâm màn hình
-                const contentElement = document.getElementById('TodoExtension');
-                contentElement.innerHTML = response;
-            }
-        };
-        xhr.open('GET', '/TodoExtension/index.html', true); 
-        xhr.send();
-    });
+    if (element.innerText == "TIME") {
+        element.addEventListener('mouseenter', function () {
+            frameURL.src = "../ClockExtension/clock.html";
+        });
+    } else if (element.innerText == "TASKS") {
+        element.addEventListener('mouseenter', function () {
+            frameURL.src = "../ToDoExtension/index.html";
+        });
+    } else if (element.innerText == "CALCULATOR") {
+        element.addEventListener('mouseenter', function () {
+            frameURL.src = "../CalculatorExtension/index.html";
+        });
+    } else if (element.innerText == "WEATHER") {
+        element.addEventListener('mouseenter', function () {
+            frameURL.src = "../WeatherExtension/index.html";
+        });
+    } else if (element.innerText == "ACCOUNT") {
+        element.addEventListener('mouseenter', function () {
+            frameURL.src = "../WeatherExtension/index.html";
+        });
+    }
 
     element.addEventListener('click', function () {
-        // Xóa nội dung khi chuột click li
-        const contentElement = document.getElementById('TodoExtension');
-        contentElement.innerHTML = '';
-
+        frameURL.src = "";
     });
 });
