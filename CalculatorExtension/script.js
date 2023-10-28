ea8e442a60f07e0a59037774b6fc0c532243a262
Hideyoshi <imlda1053@gmail.com>
2023-10-28 12:37:29 +0700

    Make the expression easier to read

---
+++ CalculatorExtension/script.js
@@ -0,0 +1,25 @@
+function log10(x) {
+    return Math.log10(x);
+}
+
+function log2(x) {
+    return Math.log2(x);
+}
+
+function ln(x) {
+    return Math.log(x);
+}
+
+function sin(x) {
+    return Math.sin(x);
+}
+
+function cos(x) {
+    return Math.cos(x);
+}
+
+function tan(x) {
+    return Math.tan(x);
+}
+const π = Math.PI
+const e = Math.E
